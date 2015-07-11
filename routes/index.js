5308c16a334a1b61de8199b0742dbb1951d17ac1
Michael Sankovich <misankovich@gmail.com>
2015-07-10 20:56:01 -0700

    states

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var firebase = require('firebase');
+var mongoose = require('mongoose');
 
-/* GET home page. */
+mongoose.connect('mongodb://localhost:27017/cartrade');
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'WheelSwap' });
+});
+
+router.get('/listings/marketplace', function (req, res, next) {
+  res.render('inventory', {title: 'WheelSwap'});
+});
+
+router.get('/myinventory', function (req, res, next) {
+  res.render('myinventory', {title: 'My Inventory'});
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', {title: 'Login Page'});
+});
+
+router.get('/listings/addcar', function(req, res, next) {
+  res.rend('addcar', {title: 'Add Car'});
+});
+
+router.get('/listings/editcar', function(req, res, next) {
+  res.rend('editcar', {title: 'Edit Car'});
+});
+
+router.get('/listings/trade', function(req, res, next) {
+  res.rend('trade', {title: 'Trade Car'});
 });
 
 module.exports = router;
