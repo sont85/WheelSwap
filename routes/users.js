508ff7007a5e4bf9deeaf696201c1302428db680
Son Truong <sont85@gmail.com>
2015-07-10 20:56:09 -0700

    google auth works but not other computer

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  console.log(req.user);
+  res.render("users", {user: { name: req.user.displayName,
+                              image: req.user._json.image.url }});
+
 });
 
 module.exports = router;
