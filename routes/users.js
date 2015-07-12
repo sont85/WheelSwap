543627c6de8694b6f1ad5f046e31760b9e4a2c5b
Son Truong <sont85@gmail.com>
2015-07-11 18:07:03 -0700

    restyle addcar and schema

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require("mongoose")
 
+
+inventorySchema = new mongoose.Schema({
+  model: String,
+  year: Number,
+  color: String,
+  condition: String,
+});
+
+userSchema = new mongoose.Schema({
+  user: {type: String, required: true},
+  inventory: [inventorySchema],
+  email: String
+
+});
+
+var User = mongoose.model("User", userSchema)
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   console.log(req.user);
