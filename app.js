52180eb149006822962b94f049c4aa5524f296de
Son Truong <sont85@gmail.com>
2015-07-14 13:05:55 -0700

    hid client secret and id with process env

--- app.js
+++ app.js
@@ -13,9 +13,8 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var app = express();
 
 passport.use(new GoogleStrategy({
-  clientID: '260657249477-biu2qgp1a8kt2d8s2f03mslneuefda38.apps.googleusercontent.com',
-  clientSecret: 'zBHF9LndcyxZbNV1YiQAHliQ',
-  // callbackURL: 'http://localhost:3000/auth/google/callback'},
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: 'https://wheelswap.herokuapp.com/auth/google/callback'},
   function(req, accessToken, refreshToken, profile, done) {
     done(null, profile);
